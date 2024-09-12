1fa4ca4c71b08c98190b0c3370c074747fe19d63
Sikha Vamsi <sikha.vamsi2004@gmail.com>
2024-09-12 19:42:29 +0530

    Update Elevator.java

--- Advanced Elevator Control System/src/Elevator.java
+++ Advanced Elevator Control System/src/Elevator.java
@@ -1,125 +1,145 @@
 import java.util.*;
-import java.util.ArrayList;
 
-// An elevator goes up , it continues to go up until there are no *dropoffs or *pickup requests in that direction
+// Enhanced Elevator System
 public class Elevator {
     private static final int MIN_FLOOR = 0;
     private static final int MAX_FLOOR = 10;
-    private static int processingTime = 500;//ms
+    private static int processingTime = 500; // ms
     private int currentFloor;
     private Direction currentDirection;
-    //keeps track of people waiting K(starting floor) : V(List of All the destination floor from that floor)
-    private Map<Integer, List<Integer>> requestedPathsMap;
-    // Once the people at a given floor have boarded the elevator,
-    // *The currentFloorDestinations array -> (keeps track of the floors the elevator will visit by setting the value at the appropriate index to true)
-    //  to implement the processFloor(),
-    // callElevator() and moveElevator() functions We also have a main function that provides an easy way to manually test your elevator implementation.
-    private Boolean[] currentFloorDestinations;
-    public Elevator() {
-        this.currentFloor = 0;//assumption the lift is starting from Ground
-        this.currentDirection = Direction.UP;//If at bottom , the lift will go up
-        this.requestedPathsMap = new HashMap<>();
-        this.currentFloorDestinations = new Boolean[MAX_FLOOR + 1];
-        Arrays.fill(this.currentFloorDestinations, Boolean.FALSE);
-    }
-    public void setProcessingTime(int processingTime) {
-        Elevator.processingTime = processingTime;
-    }
-    public int getCurrentFloor() {
-        return this.currentFloor;
-    }
-    public Map<Integer, List<Integer>> getRequestedPathsMap() {
-        return this.requestedPathsMap;
-    }
-    public Boolean[] getCurrentFloorDestinations() {
-        return this.currentFloorDestinations;
+    private boolean[] currentFloorDestinations;  // Tracks floor destinations
+    private PriorityQueue<Request> requestQueue; // Priority-based requests
 
+    public Elevator() {
+        this.currentFloor = 0;
+        this.currentDirection = Direction.UP;
+        this.currentFloorDestinations = new boolean[MAX_FLOOR + 1];
+        this.requestQueue = new PriorityQueue<>(Comparator.comparingInt(Request::getPriority)); // Prioritize requests
     }
+
     public void start() throws InterruptedException {
-        currentDirection = Direction.UP;//Assumption the lift is on ground floor initially
-    do {
-        System.out.println("--------");
-        processFloor(currentFloor);
-        System.out.println("--------");
-    } while (currentDirection != Direction.IDLE);
-        System.out.println("No one is waiting and " +
-        "no one is looking to go anywhere");
-        System.out.println("Chilling for now");
+        currentDirection = Direction.UP;
+        do {
+            System.out.println("--------");
+            processFloor(currentFloor);
+            System.out.println("--------");
+        } while (currentDirection != Direction.IDLE);
+        System.out.println("No more requests, elevator is idle.");
     }
-    public void lunchtimeElevatorRush() {
-        Random random = new Random();
-        for (int i = 0; i < 30; i++) {
-        callElevator(random.nextInt(11),
-        random.nextInt(10) + 1);
-    }
-    }
-    // TO DO #1
-    public void callElevator(int start, int destination) {
+
+    public void callElevator(int start, int destination, int priority) {
         if (isInvalidFloor(start) || isInvalidFloor(destination) || start == destination) {
-        System.out.println("INVALID FLOORS. Try again");
-        return;
-    }
-        if (requestedPathsMap.containsKey(start)){//if already START is in map, add the destination in the list
-            requestedPathsMap.get(start).add(destination);
+            System.out.println("INVALID FLOORS. Try again");
+            return;
         }
-        else {//else add the new key aas START with the list containing our DESTINATION
-            requestedPathsMap.put(start, new ArrayList<Integer>() 
-            {{ 
-                add(destination); 
-            }});
-        }
+        requestQueue.add(new Request(start, destination, priority));  // Add prioritized requests
     }
-    // TO DO #2
+
     private void processFloor(int floor) throws InterruptedException {
-        if (currentFloorDestinations[floor]){ //Deboarding if any people who reached this destination floor
-        System.out.println("UN-BOARDING at Floor : " + floor);
+        if (currentFloorDestinations[floor]) {
+            System.out.println("UN-BOARDING at Floor: " + floor);
         }
-        if (requestedPathsMap.containsKey(floor)) {//Onboarding people and theirdestination
-            System.out.println("BOARDING at Floor : " + floor);
-            requestedPathsMap.get(floor).forEach(destinationFloor -> currentFloorDestinations[destinationFloor] = true);//Marked true for next traversals
-            requestedPathsMap.remove(floor);//removing the entry from map as we have marked all the destination
+
+        // Process requests at the current floor
+        Request currentRequest = requestQueue.peek();
+        if (currentRequest != null && currentRequest.getStartFloor() == floor) {
+            System.out.println("BOARDING at Floor: " + floor);
+            currentFloorDestinations[currentRequest.getDestinationFloor()] = true;
+            requestQueue.poll();  // Remove request after boarding
         }
-        currentFloorDestinations[floor] = false;//Marked false as we are just arrived in the current floor
+
+        currentFloorDestinations[floor] = false;  // Done with this floor
         moveElevator();
     }
-    //TO DO #3
-    private void moveElevator() throws InterruptedException {
-        //SETIING OF DIRECTION
-        //IDELING the elevator
-        if (!Arrays.asList(currentFloorDestinations).contains(true) && requestedPathsMap.isEmpty()) {//STOPPING THE ELEVATOR :checking destinations are reached and request list is empty
-            currentDirection = Direction.IDLE;//this will break the while loop in our initial start() method
-            return;
 
-        } else if (isInvalidFloor(currentFloor + 1)) {//SWITCH TO DOWN direction when reached top floor
+    private void moveElevator() throws InterruptedException {
+        if (!Arrays.asList(currentFloorDestinations).contains(true) && requestQueue.isEmpty()) {
+            currentDirection = Direction.IDLE;
+            return;
+        } else if (isInvalidFloor(currentFloor + 1)) {
             currentDirection = Direction.DOWN;
-        } else if (isInvalidFloor(currentFloor - 1)) {//SWITCH TO UP direction when reached bottom floor
+        } else if (isInvalidFloor(currentFloor - 1)) {
             currentDirection = Direction.UP;
         }
-        switch (currentDirection) {//Move the elevator Enhanced switch available only in JDK14 onwards case UP-> moveUp();
-            case UP: {
-            moveUp();
-            break;
-            }
-            case DOWN: {
-            moveDown();
-            break;
-            }
-            default: {
-            System.out.println("Elevator Malfunctioned");
-            }
+
+        switch (currentDirection) {
+            case UP:
+                moveUp();
+                break;
+            case DOWN:
+                moveDown();
+                break;
+            default:
+                System.out.println("Elevator Malfunctioned");
         }
     }
+
     private void moveUp() throws InterruptedException {
         currentFloor++;
         System.out.println("GOING UP TO " + currentFloor);
         Thread.sleep(processingTime);
     }
+
     private void moveDown() throws InterruptedException {
         currentFloor--;
         System.out.println("GOING DOWN TO " + currentFloor);
         Thread.sleep(processingTime);
     }
+
     private boolean isInvalidFloor(int floor) {
         return floor < MIN_FLOOR || floor > MAX_FLOOR;
+    }
+}
+
+class Request {
+    private final int startFloor;
+    private final int destinationFloor;
+    private final int priority;  // Lower values mean higher priority
+
+    public Request(int start, int destination, int priority) {
+        this.startFloor = start;
+        this.destinationFloor = destination;
+        this.priority = priority;
+    }
+
+    public int getStartFloor() {
+        return startFloor;
+    }
+
+    public int getDestinationFloor() {
+        return destinationFloor;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+}
+
+// ElevatorManager to handle multiple elevators
+class ElevatorManager {
+    private List<Elevator> elevators;
+
+    public ElevatorManager(int numElevators) {
+        elevators = new ArrayList<>(numElevators);
+        for (int i = 0; i < numElevators; i++) {
+            elevators.add(new Elevator());
+        }
+    }
+
+    public void assignRequest(int start, int destination, int priority) {
+        // Assign request to the nearest available elevator
+        Elevator bestElevator = findBestElevator(start);
+        bestElevator.callElevator(start, destination, priority);
+    }
+
+    private Elevator findBestElevator(int startFloor) {
+        // Simple heuristic to find the nearest elevator (could be improved)
+        return elevators.get(0);  // Placeholder, logic to find best elevator can be added
+    }
+
+    public void startAllElevators() throws InterruptedException {
+        for (Elevator elevator : elevators) {
+            elevator.start();
+        }
     }
 }
